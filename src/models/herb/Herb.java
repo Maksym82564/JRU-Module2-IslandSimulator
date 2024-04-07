90cc280d2ede27c1356eef044086af1c95415535
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-04-07 13:10:10 +0300

    Added class for herbs

--- src/models/herb/Herb.java
+++ src/models/herb/Herb.java
@@ -2,18 +2,26 @@ package models.herb;
 
 import interfaces.Entity;
 import models.map.Coordinates;
+import models.map.IslandMap;
 
 public class Herb implements Entity {
     private static final String ICON = "\uD83C\uDF3F";
     private static final String name = "herb";
     private final Coordinates coords;
+    private final IslandMap islandMap;
 
-    public Herb(Coordinates coords) {
+    public Herb(Coordinates coords, IslandMap islandMap) {
         this.coords = coords;
+        this.islandMap = islandMap;
+        islandMap.getArea(coords).addEntity(this);
     }
 
     public Coordinates getCoords() {
         return coords;
+    }
+
+    public IslandMap getIslandMap() {
+        return islandMap;
     }
 
     @Override
