e7cc6affbfce1dd2a0bb8b1f81ee4994cd20c45f
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-04-08 17:33:28 +0300

    Made wrapper class for creation and running animal threads

---
+++ src/app/Wildlife.java
@@ -0,0 +1,47 @@
+package app;
+
+import enums.HerbivorousEnum;
+import enums.PredatorEnum;
+import factory.AnimalFactory;
+import factory.HerbFactory;
+import interfaces.Entity;
+import models.animal.Animal;
+import models.herb.Herb;
+import models.map.IslandMap;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Wildlife {
+    private final LifeCreator lifeCreator;
+    private final IslandMap islandMap;
+
+    public Wildlife(LifeCreator lifeCreator, IslandMap islandMap) {
+        this.lifeCreator = lifeCreator;
+        this.islandMap = islandMap;
+    }
+
+    private Set<Entity> createEntities(int herbAmount, int predatorAmount, int herbivorousAmount) {
+        Set<Animal> predatorSet = AnimalFactory.createAnimalSet(predatorAmount, islandMap, PredatorEnum.class);
+        Set<Animal> herbivorousSet = AnimalFactory.createAnimalSet(herbivorousAmount, islandMap, HerbivorousEnum.class);
+        Set<Herb> herbs = HerbFactory.createHerbs(herbAmount, islandMap);
+        Set<Entity> entities = new HashSet<>();
+        entities.addAll(predatorSet);
+        entities.addAll(herbivorousSet);
+        entities.addAll(herbs);
+        return entities;
+    }
+
+    public void createWildlife(int herbAmount, int predatorAmount, int herbivorousAmount) {
+        Set<Entity> entities = createEntities(herbAmount, predatorAmount, herbivorousAmount);
+        lifeCreator.animateNewborn(entities);
+    }
+
+    public LifeCreator getLifeCreator() {
+        return lifeCreator;
+    }
+
+    public void endWildlife() {
+        lifeCreator.stop();
+    }
+}
