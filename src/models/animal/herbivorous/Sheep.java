683c092d1319cee438e890bb1f8dfc0ca618ea88
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-04-09 06:06:38 +0300

    Nutrition and satiety variables for animals added

--- src/models/animal/herbivorous/Sheep.java
+++ src/models/animal/herbivorous/Sheep.java
@@ -3,12 +3,12 @@ package models.animal.herbivorous;
 import models.map.Coordinates;
 import models.map.IslandMap;
 
-import java.util.Map;
-
 public class Sheep extends Herbivorous {
     private static final String ICON = "\uD83D\uDC11";
     private static final String NAME = "sheep";
     private static final int MAX_SPEED = 3;
+    private static final int MAX_SATIETY = 8;
+    private static final int NUTRITION_VALUE = 50;
 
     public Sheep(IslandMap islandMap, Coordinates coords) {
         super(islandMap, coords);
@@ -16,5 +16,7 @@ public class Sheep extends Herbivorous {
         setIcon(ICON);
         setMaxSpeed(MAX_SPEED);
         setChanceToConsumeMap(CONSUME_MAP);
+        setMaxSatiety(MAX_SATIETY);
+        setNutritionValue(NUTRITION_VALUE);
     }
 }
