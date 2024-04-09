d46ad08d639d5238070fcba7de8090d42f1e19a0
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-04-09 11:27:57 +0300

    Thread handling of animals created by reproduction

--- src/app/LifeCreator.java
+++ src/app/LifeCreator.java
@@ -1,6 +1,7 @@
 package app;
 
 import interfaces.Entity;
+import interfaces.ReproductionHandler;
 import models.animal.Animal;
 
 import java.util.HashSet;
@@ -9,33 +10,48 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class LifeCreator {
-    private final ScheduledExecutorService executorService;
+public class LifeCreator implements ReproductionHandler {
+    private final ScheduledExecutorService newbornEntityExecutor;
+    private final ScheduledExecutorService reproducedAnimalExecutor;
     private final Set<Entity> newbornEntities = new HashSet<>();
+    private final Set<Animal> reproducedAnimals = Animal.getReproducedAnimals();
 
     public LifeCreator() {
         int processorsNum = Runtime.getRuntime().availableProcessors();
-        this.executorService = Executors.newScheduledThreadPool(processorsNum - 1);
+        this.newbornEntityExecutor = Executors.newScheduledThreadPool(processorsNum - 1);
+        this.reproducedAnimalExecutor = Executors.newScheduledThreadPool(1);
     }
 
-    public void animateNewborn(Animal animal) {
-        newbornEntities.add(animal);
-        executorService.scheduleAtFixedRate(animal,0, 1, TimeUnit.SECONDS);
+    @Override
+    public void run() {
+        animateReproduced();
     }
 
-    public void animateNewborn(Set<Entity> entities) {
+    public void animateReproduced() {
+        Set<Animal> copiedReproducedAnimals = new HashSet<>(reproducedAnimals);
+        for (Animal reproducedAnimal : copiedReproducedAnimals) {
+            newbornEntities.add(reproducedAnimal);
+            newbornEntityExecutor.scheduleAtFixedRate(reproducedAnimal,0, 1000, TimeUnit.MILLISECONDS);
+            reproducedAnimals.remove(reproducedAnimal);
+
+        }
+    }
+
+    public void getReproductionHandlerStarted() {
+        reproducedAnimalExecutor.scheduleAtFixedRate(this, 500, 1000, TimeUnit.MILLISECONDS);
+    }
+
+    public void animateNewborns(Set<Entity> entities) {
         newbornEntities.clear();
         newbornEntities.addAll(entities);
         for (Entity entity : entities) {
-            if (entity instanceof Animal animal) {
-                animal.setLifeCreator(this);
-            }
-            executorService.scheduleAtFixedRate(entity,500, 1000, TimeUnit.MILLISECONDS);
+            newbornEntityExecutor.scheduleAtFixedRate(entity,500, 1000, TimeUnit.MILLISECONDS);
         }
     }
 
     public void stop() {
-        executorService.shutdownNow();
+        newbornEntityExecutor.shutdownNow();
+        reproducedAnimalExecutor.shutdownNow();
     }
 
     public Set<Entity> getNewbornEntities() {
